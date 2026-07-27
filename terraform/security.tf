c60bf7edd669de01ae3fbbec51f344ef5c67751d
Amir Fakerov <82692159+Zhenyaof@users.noreply.github.com>
2026-07-28 00:19:46 +0330

    Define network security rules for SSH, HTTP, and HTTPS
    Added security rules to allow SSH, HTTP, and HTTPS traffic.

--- terraform/security.tf
+++ terraform/security.tf
@@ -7,3 +7,54 @@ resource "azurerm_network_security_group" "nsg" {
 
   tags = local.common_tags
 }
+
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "Allow-SSH"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+
+  source_port_range           = "*"
+  destination_port_range      = "22"
+
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.nsg["management"].name
+}
+
+resource "azurerm_network_security_rule" "http" {
+  name                        = "Allow-HTTP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+
+  source_port_range           = "*"
+  destination_port_range      = "80"
+
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.nsg["web"].name
+}
+
+resource "azurerm_network_security_rule" "https" {
+  name                        = "Allow-HTTPS"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+
+  source_port_range           = "*"
+  destination_port_range      = "443"
+
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.nsg["web"].name
+}
