f61648b9b0eef572fe85386a5f5c476590ee275e
Amir Fakerov <82692159+Zhenyaof@users.noreply.github.com>
2026-07-28 00:12:12 +0330

    Enhance locals with project name and NSGs
    Added project_name and network security groups to locals.

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,8 +1,28 @@
 locals {
+  project_name = "hyrcania"
+
   common_tags = {
     Project     = "Hyrcania"
     Environment = "Lab"
     ManagedBy   = "Terraform"
     Owner       = "Amir"
+  }
+
+  nsgs = {
+    management = {
+      name = "management"
+    }
+
+    web = {
+      name = "web"
+    }
+
+    application = {
+      name = "application"
+    }
+
+    database = {
+      name = "database"
+    }
   }
 }
