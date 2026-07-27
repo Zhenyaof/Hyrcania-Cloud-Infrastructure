c133650a11dfc16663f76fce405e72cfa1bf4170
Amir Fakerov <82692159+Zhenyaof@users.noreply.github.com>
2026-07-27 21:30:42 +0330

    Refactor networking.tf to include subnets and common tags
    Updated the tags for the virtual network resource to use local.common_tags instead of hardcoded values. Added multiple subnet resources for management, web, application, database, and bastion.

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -1,13 +1,59 @@
 resource "azurerm_virtual_network" "main" {
-
   name                = var.vnet_name
   location            = var.location
   resource_group_name = azurerm_resource_group.main.name
 
   address_space = var.vnet_address_space
 
-  tags = {
-    Project   = "Hyrcania"
-    ManagedBy = "Terraform"
-  }
+  tags = local.common_tags
+}
+
+resource "azurerm_subnet" "management" {
+  name                 = "Management"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+
+  address_prefixes = [
+    "10.0.1.0/24"
+  ]
+}
+
+resource "azurerm_subnet" "web" {
+  name                 = "Web"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+
+  address_prefixes = [
+    "10.0.2.0/24"
+  ]
+}
+
+resource "azurerm_subnet" "application" {
+  name                 = "Application"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+
+  address_prefixes = [
+    "10.0.3.0/24"
+  ]
+}
+
+resource "azurerm_subnet" "database" {
+  name                 = "Database"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+
+  address_prefixes = [
+    "10.0.4.0/24"
+  ]
+}
+
+resource "azurerm_subnet" "bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+
+  address_prefixes = [
+    "10.0.5.0/24"
+  ]
 }
