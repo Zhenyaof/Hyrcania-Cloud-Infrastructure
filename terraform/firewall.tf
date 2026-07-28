6517afc08e477dfa7972de73ff61a68c40a13cf4
Amir Fakerov <82692159+Zhenyaof@users.noreply.github.com>
2026-07-28 16:15:17 +0330

    Add Azure Load Balancer infrastructure

---
+++ terraform/firewall.tf
@@ -0,0 +1,27 @@
+resource "azurerm_public_ip" "firewall" {
+  name                = "${local.project_name}-firewall-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+
+  tags = local.common_tags
+}
+
+resource "azurerm_firewall" "main" {
+  name                = "${local.project_name}-firewall"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  sku_name = "AZFW_VNet"
+  sku_tier = "Standard"
+
+  ip_configuration {
+    name                 = "firewall-ip-config"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.firewall.id
+  }
+
+  tags = local.common_tags
+}
