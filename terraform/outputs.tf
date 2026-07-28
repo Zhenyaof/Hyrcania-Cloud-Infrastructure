5a5420a2a9c416583c350d590ae7d66442d12545
Amir Fakerov <82692159+Zhenyaof@users.noreply.github.com>
2026-07-28 16:18:07 +0330

    Add Azure Load Balancer infrastructure

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -77,3 +77,17 @@ output "load_balancer_name" {
 output "load_balancer_public_ip" {
   value = azurerm_public_ip.lb.ip_address
 }
+output "application_gateway_name" {
+  value = azurerm_application_gateway.main.name
+}
+
+output "application_gateway_public_ip" {
+  value = azurerm_public_ip.appgw.ip_address
+}
+output "firewall_name" {
+  value = azurerm_firewall.main.name
+}
+
+output "firewall_public_ip" {
+  value = azurerm_public_ip.firewall.ip_address
+}
