102148d63af2627e0cea4a37f0821763f5829438
Amir Fakerov <82692159+Zhenyaof@users.noreply.github.com>
2026-07-28 23:54:46 +0330

    Add Azure Load Balancer infrastructure

---
+++ terraform/vpn-gateway.tf
@@ -0,0 +1,33 @@
+resource "azurerm_public_ip" "vpn" {
+  name                = "${local.project_name}-vpn-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+
+  tags = local.common_tags
+}
+
+resource "azurerm_virtual_network_gateway" "main" {
+  name                = "${local.project_name}-vpn-gateway"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  sku = "VpnGw1"
+
+  active_active = false
+  enable_bgp    = false
+
+  ip_configuration {
+    name                          = "gateway-config"
+    public_ip_address_id          = azurerm_public_ip.vpn.id
+    subnet_id                     = azurerm_subnet.bastion.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = local.common_tags
+}
