a3c2dc4f3d79ac7301ab7719d6f2f725b8275d74
Amir Fakerov <82692159+Zhenyaof@users.noreply.github.com>
2026-08-09 00:25:46 +0330

    Format Terraform configuration

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "main" {
-  name                     = "hyrcaniastorage01"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = var.location
+  name                = "hyrcaniastorage01"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
 
   account_tier             = "Standard"
   account_replication_type = "LRS"
